f1f35e091743b9d68253e463cee52e07cda8e655
Fabrice Dall'ara <fabrice.dallara@hotmail.com>
2023-08-02 19:06:25 +0200

    Debugging for_each statement

--- module/main.tf
+++ module/main.tf
@@ -26,8 +26,8 @@ resource "azurerm_virtual_desktop_host_pool" "this" {
     dynamic "schedule" {
       for_each = var.schedule_agent_updates_schedules
       content {
-        day_of_week = schedule.value["day_of_week"]
-        hour_of_day = schedule.value.["hour_of_day"]
+        day_of_week = schedule.value.day_of_week
+        hour_of_day = schedule.value.hour_of_day
       }
     }
   }
