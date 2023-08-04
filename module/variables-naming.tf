e3e98bec7d46017c9b429cc98e68c4e5fb2b6a53
Fabrice Dall'ara <fabrice.dallara@hotmail.com>
2023-08-04 08:36:18 +0200

    Terraform fmt

--- module/variables-naming.tf
+++ module/variables-naming.tf
@@ -18,7 +18,7 @@ variable "workload_info" {
     condition     = var.workload_info == "" || can(regex("^[a-z0-9]([a-z0-9-]*[a-z0-9])?$", var.workload_info))
     error_message = "Invalid variable: ${var.workload_info}. Variable name must start with a lowercase letter, end with an alphanumeric lowercase character, and contain only lowercase letters, digits, or a dash (-)."
   }
-  
+
 }
 
 variable "landing_zone_slug" {
